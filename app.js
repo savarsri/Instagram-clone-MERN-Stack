cda6a33654f148c2416cc9b64e8e6625648fad0c
Savar Srivastava <savarsri@gmail.com>
2024-04-23 10:04:00 +0530

    Fixed vulnerabilities in Frontend

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express()
 const mongoose  = require('mongoose')
 const PORT = process.env.PORT || 5000
 const {MONGOURI} = require('./config/keys')
+const dotenv = require('dotenv')
 
 
-mongoose.connect(MONGOURI,{
+mongoose.connect("mongodb://localhost:27017/ost",{
     useNewUrlParser:true,
     useUnifiedTopology: true
 
